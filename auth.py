6098c25bceef8e71c316f92d06a036c4f099e3b5
100522320 <100522320@alumnos.uc3m.es>
2025-10-20 09:32:25 +0200

    Arreglar los imports

--- auth.py
+++ auth.py
@@ -3,7 +3,7 @@ import json # Para leer/escribir users.json
 
 # Para el hashing de la contraseña (Registro de usuario)
 from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
-#from cryptography.hazmat.primitives.kdf.pbkdf2 import InvalidKeyError
+from cryptography.exceptions import InvalidKey
 from cryptography.hazmat.primitives import hashes
 
 # Para derivar la clave de cifrado K (una vez autenticado)
@@ -147,10 +147,10 @@ def autenticar_usuario(nombre_usuario, contraseña):
 
         return True 
         
-    #except InvalidKeyError:
+    except InvalidKey:
         # Esto ocurre cuando la contraseña introducida es incorrecta.
-        #print(f"Fallo de autenticación: Contraseña incorrecta para '{nombre_usuario}'.")
-        #return False
+        print(f"Fallo de autenticación: Contraseña incorrecta para '{nombre_usuario}'.")
+        return False
         
     except Exception as e:
         # Otros errores durante el proceso de verificación.
